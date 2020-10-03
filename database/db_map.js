1a59e43b5fd555289103c20bf56fb54da4ecf9cf
Adam Elaoud <adam.elaoud@gmail.com>
2020-10-03 16:51:41 +0100

    added level updating to editing

--- database/db_map.js
+++ database/db_map.js
@@ -1,18 +1,33 @@
 module.exports = {
     fields: [
+        // BASE FIELDS
         "_school",
         "_lvl",
         "_xp",
         "_patron",
+        
+        // GOLD
         "gold._total",
+
+        // FISH
         "fish._total",
         "fish._caught",
+
+        // REAGENTS
         "reagents._total",
         "reagents._collected",
+
+        // TCs
         "TCs._total",
         "TCs._collected",
+
+        // RECIPES
         "recipes._total",
+
+        // SPELLS
         "spells._total",
+
+        // ITEMS
         "items._total"
     ],
     arrays: [
--- database/edit_user.js
+++ database/edit_user.js
@@ -1,7 +1,7 @@
 require("mongodb");
 const MongoConnector = require("../util/mongo.js");
 const ErrorLog = require("../util/errors.js");
-
+const Levels = require("../game_data/levels.js");
 
 module.exports = {
     name: "user data manipulation",
@@ -14,10 +14,15 @@ module.exports = {
             const users = db.collection("users");
 
             // correct value typing
-            if (field === "_patron" || field === "debuffs.beguiled.val")
+            if (field === "_patron" || field === "debuffs.beguiled.val") {
                 val = (val === "true");
-            else if (field !== "_school")
+            } else if (field === "_xp") {
+                module.exports.checkLevel(bot, msg, op, val, userID);
+                // return, as checkLevel function updates level and xp as necessary
+                return;
+            } else if (field !== "_school") {
                 val = parseInt(val);
+            }
             
 
             // if action is set
@@ -42,7 +47,7 @@ module.exports = {
             }            
 
         } catch (err) {
-            ErrorLog.log(bot, msg, `${op}${field} [${val}] to <@${userID}>`, err);
+            ErrorLog.log(bot, msg, `${field} **[${op}${val}]** to <@${userID}>`, err);
 
         } finally {
             dbClient.close();
@@ -56,10 +61,58 @@ module.exports = {
             const users = db.collection("users");
 
             // insert array elements here
-            console.log(`inserting element ${op}${field} [${val}] to <@${userID}>`);
+            console.log(`inserting element ${field} **[${op}${val}]** to <@${userID}>`);
 
         } catch (err) {
-            ErrorLog.log(bot, msg, `${op}${field} [${val}] to <@${userID}>`, err);
+            ErrorLog.log(bot, msg, `${field} **[${op}${val}]** to <@${userID}>`, err);
+
+        } finally {
+            dbClient.close();
+        }
+    },
+    async checkLevel(bot, msg, op, val, userID){
+        const dbClient = MongoConnector.client();
+        
+        try {
+            const db = await MongoConnector.connect(bot, msg, "ZanderDB", dbClient);
+            const users = db.collection("users");
+
+            // collect current user data
+            const user = await users.findOne({ "_user": userID });
+            const level = user._lvl;
+            const xp = user._xp;
+            let remainingXP = 0;
+
+            // set iterating values
+            if (op === "+")
+                remainingXP = xp + parseInt(val);
+            else if (op === "=")
+                remainingXP = parseInt(val);
+            else
+                throw `invalid operation provided: ${op}`
+
+            let costToLevel = Levels.costs[level + 1];
+            let newLevel = level;
+
+            // if current xp meets level up requirements, level up and iterate
+            while (remainingXP >= costToLevel) {
+                newLevel++;
+                remainingXP -= costToLevel;
+                costToLevel = Levels.costs[newLevel + 1];
+            }
+            
+            await users.findOneAndUpdate(
+                { "_user": userID },
+                { 
+                    $set: {
+                        ["_xp"]: remainingXP,
+                        ["_lvl"]: newLevel
+                    }
+                }
+            );
+
+        } catch (err) {
+            ErrorLog.log(bot, msg, `checking ${op}${val} xp for <@${userID}>`, err);
 
         } finally {
             dbClient.close();
